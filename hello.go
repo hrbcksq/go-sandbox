d68300b8e934e240c946ad2433702b3a3e5e791f
Ruslan Kisly <rchisly@gmail.com>
2018-07-13 15:32:00 +0300

    Short variable declarations

--- hello.go
+++ hello.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 )
 
-var i, j int = 1, 2
-
 func main() {
-	var c, python, java = true, false, "no!"
+	var i, j int = 1, 2
+	// the := short assignment statement can be used in place of a var declaration with implicit type
+	k := 3
+	c, python, java := true, false, "no!"
+
 	fmt.Println(i, j, c, python, java)
 }
+
+// Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
