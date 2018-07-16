5592982e42ad83f8a8eb59b051c0aff4908797dc
Ruslan Kisly <rchisly@gmail.com>
2018-07-16 16:59:30 +0300

    Defer

--- hello.go
+++ hello.go
@@ -2,22 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	fmt.Println("What time is it?")
-	now := time.Now().Hour()
-	switch {
-	case now < 8 && now > 21:
-		fmt.Println("Good night")
-	case now < 12:
-		fmt.Println("Good morning")
-	case now < 14:
-		fmt.Println("Good afternoon")
-	case now < 18:
-		fmt.Println("Good evening")
-	default:
-		fmt.Println("Too far away.")
-	}
+	defer fmt.Println("world")
+	fmt.Println("hello")
 }
