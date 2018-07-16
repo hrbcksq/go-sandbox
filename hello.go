ce1ff8768ce193108227bb4d011ea0ace2671ca3
Ruslan Kisly <rchisly@gmail.com>
2018-07-16 14:35:45 +0300

    If condition

--- hello.go
+++ hello.go
@@ -1,6 +1,22 @@
 package main
 
-func main() {
-	for {
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) string {
+	var result string
+	if x < 0 {
+		result = fmt.Sprint(complex(0, math.Sqrt(-x)))
+	} else {
+		result = fmt.Sprint(math.Sqrt(x))
 	}
+	return result
+}
+
+func main() {
+	sqrt1 := sqrt(40)
+	sqrt2 := sqrt(-2)
+	fmt.Println(sqrt1, sqrt2)
 }
