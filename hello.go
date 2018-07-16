814007c2a3c23e151d82f45240d8ab88a2c7787e
Ruslan Kisly <rchisly@gmail.com>
2018-07-16 16:45:04 +0300

    Switch

--- hello.go
+++ hello.go
@@ -2,27 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-// Sqrt is the custom function
-func Sqrt(x float64) float64 {
-	epsilon := 1e-160
-	z := 1.0
-	for prev := 0.0; math.Abs(z-prev) > epsilon; {
-		prev = z
-		for index := 0; index < 10; index++ {
-			z -= (z*z - x) / (2 * z)
-		}
-		fmt.Println(z)
+func main() {
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Println("%s.", os)
 	}
 
-	return z
-}
-
-func main() {
-	origin := 40.0
-	result := Sqrt(origin)
-	diff := math.Abs(math.Pow(result, 2) - origin)
-	fmt.Printf("The result is: %v\nThe difference: is %v", result, diff)
 }
